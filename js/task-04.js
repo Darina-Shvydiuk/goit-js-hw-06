e31254baa17a84271f476a8646adee29029e0c5d
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-08-02 21:49:38 +0300

    fix 4 task

--- js/task-04.js
+++ js/task-04.js
@@ -3,17 +3,12 @@ const minusButton = document.querySelector(`[data-action="decrement"]`);
 const plusButton = document.querySelector(`[data-action="increment"]`);
 const value = document.querySelector("#value");
 
-const handleTargetBtnPlusClick = () => {
-  value.textContent = counterValue;
-
-  return (counterValue += 1);
-};
-
 plusButton.addEventListener("click", handleTargetBtnPlusClick);
-
-const handleTargetBtnMinusClick = () => {
-  value.textContent = counterValue;
-  return (counterValue -= 1);
-};
+function handleTargetBtnPlusClick() {
+  return (value.textContent = counterValue += 1);
+}
 
 minusButton.addEventListener("click", handleTargetBtnMinusClick);
+function handleTargetBtnMinusClick() {
+  return (value.textContent = counterValue -= 1);
+}
