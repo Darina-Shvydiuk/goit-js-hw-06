4ba3e157f97546d3736b05486b16b76dc0d24c4c
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-08-02 20:16:33 +0300

    fix task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,10 @@
 const input = document.querySelector("input");
-console.log(input);
+
 const span = document.querySelector("span");
-console.log(span);
 
 input.addEventListener("input", (event) => {
-  if (input) {
-    span.textContent = event.target.value;
-  } else {
+  span.textContent = event.currentTarget.value;
+  if (span.textContent === "") {
     span.textContent = "Anonymous";
   }
 });
